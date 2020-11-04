5d1d3b49b63c6246fa9303f549a8c6e3c2058831
mubarak23 <mubarakaminu340@gmail.com>
2020-11-04 14:44:58 +0100

    home screen setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from "./logo.svg";
-import "./App.css";
+//import logo from "./logo.svg";
+//import "./App.css";
+import "./style.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./screens/HomeScreen";
 import Ticket from "./screens/TicketScreen";
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
-import Navbar from "../components/Navbar";
+//import Navbar from "../components/Navbar";
+import "../App.css";
 import { Container, Row, Col } from "react-bootstrap";
+import "../style.css";
 
 const HomeScreen = ({ history }) => {
   const redirect = "/";
@@ -12,17 +14,113 @@ const HomeScreen = ({ history }) => {
   }, [history, redirect]);
   return (
     <div>
-      <Navbar />
-      <Container>
-        <Row>
-          <Col md={2}></Col>
-          <Col md={10}>
-            <div>
-              <h2>Welcome to Home screen </h2>
+      <div className="sidebar">
+        <a className="active" href="posdashboard.html">
+          ISSUE TYPE
+        </a>
+        <a href="actions.html">ACTIONS</a>
+        <a href="status.html">STATUS</a>
+        <a href="history.html">HISTORY</a>
+      </div>
+      <div className="navbar">
+        <div className="container">
+          <div class="user">
+            <div className="user1" style={{ display: "inline-block" }}>
+              <h6>WELCOME</h6>
             </div>
-          </Col>
-        </Row>
-      </Container>
+            <div
+              className="user2"
+              style={{ display: "inline-block" }}
+              //style={{ text-transform: "uppercase" }}
+              //style="display: inline-block; text-transform: uppercase;"
+            >
+              <h6 id="username"></h6>
+            </div>
+          </div>
+          <button
+            type="submit"
+            class="btn signout"
+            id="pos-signout"
+            onclick="signOut()"
+          >
+            SIGN OUT
+          </button>
+        </div>
+      </div>
+      <div className="head_links">
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-3">
+              <a href="posdashboard.html">
+                <div className="head_link1 pos-container">
+                  <div className="lhs3" style={{ display: "inline-block" }}>
+                    <h6>POS</h6>
+                  </div>
+                  <div className="rhs3" style={{ display: "inline-block" }}>
+                    <img
+                      alt="pos"
+                      src={process.env.PUBLIC_URL + "/img/9.jpg"}
+                      //src="../../public/img/9.jpg"
+                      className="img-fluid pos-image"
+                    />
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div className="col-sm-3">
+              <a href="transferdashboard.html">
+                <div className="head_link2">
+                  <div className="lhs3" style={{ display: "inline-block" }}>
+                    <h6>TRANSFER</h6>
+                  </div>
+                  <div className="rhs3" style={{ display: "inline-block" }}>
+                    <img
+                      alt="transfer"
+                      src={process.env.PUBLIC_URL + "/img/10.jpg"}
+                      //src="assets/img/10.jpg"
+                      className="img-fluid pos-image1"
+                    />
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div className="col-sm-3">
+              <a href="walletsdashboard.html">
+                <div className="head_link4 pos-container">
+                  <div className="lhs3" style={{ display: "inline-block" }}>
+                    <h6>WALLET</h6>
+                  </div>
+                  <div className="" style={{ display: "inline-block" }}>
+                    <img
+                      alt="wallet"
+                      src={process.env.PUBLIC_URL + "/img/12.jpg"}
+                      //src="assets/img/12.jpg"
+                      className="img-fluid pos-image2"
+                    />
+                  </div>
+                </div>
+              </a>
+            </div>
+            <div className="col-sm-3">
+              <a href="othersdashboard.html">
+                <div className="head_link3 pos-container">
+                  <div className="lhs3" style={{ display: "inline-block" }}>
+                    <h6>OTHERS</h6>
+                  </div>
+                  <div className="rhs3" style={{ display: "inline-block" }}>
+                    <img
+                      alt="others"
+                      src={process.env.PUBLIC_URL + "/img/11.jpg"}
+                      //src="assets/img/11.jpg"
+                      className="img-fluid pos-image3"
+                    />
+                  </div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>{" "}
     </div>
   );
 };
